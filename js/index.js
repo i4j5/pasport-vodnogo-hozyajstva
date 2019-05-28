dfedf8113ef2fc01a3d395768b55b7b84d4c132c
Aleksandr Sazhin <i4j5@ya.ru>
2019-05-28 13:34:25 +0300

    roistat

--- js/index.js
+++ js/index.js
@@ -109,6 +109,9 @@ $(document).ready(()=>{
 	        var form = $(form),
 	        str = form.serialize()
 
+	        let roistat = window.roistat.visit || null
+	        str = str + '&roistat=' + roistat
+
 	        let btn = form.children("[type='submit']")
 	        //let btnText = btn.val()
 	        //btn.val('Обработка...')
@@ -117,7 +120,7 @@ $(document).ready(()=>{
 	        let download = form.children("[name='download']").val()
 
 	        $.ajax({
-	          url: 'http://lp.bk-invent.ru/send.php',
+	          url: '//lp.bk-invent.ru/send.php',
 	          type: 'post',
 	          data: str
 	        })
@@ -132,6 +135,7 @@ $(document).ready(()=>{
 	           }
 	           yaCounter51663104.reachGoal(yatarget)
 	           yatarget = 'order'
+	           yaCounter53737453.reachGoal('site')
 	        })
 	        .always(function() {
 			   //btn.val(btnText)
